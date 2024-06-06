dd7b06fe5ec1910fc9034b78558a7e018ade6024
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-06 22:28:23 +0200

    change train_model

--- face_rec/train_model.py
+++ face_rec/train_model.py
@@ -1,44 +1,51 @@
-import cv2
-import os
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # Install opencv-python
 import numpy as np
-from PIL import Image
 
-# Create LBPH Face Recognizer
-#pip install opencv-contrib-python
-recognizer = cv2.face.LBPHFaceRecognizer_create()
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
-# Load the Haar Cascade Classifier for face detection
-detector = cv2.CascadeClassifier("../models/haar.xml")
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
 
-def getImagesAndLabels(path):
-    imagePaths = [os.path.join(path, f) for f in os.listdir(path) if not f.startswith('.')]
-    faceSamples = []
-    Ids = []
-    names = []
-    ages = []
-    for imagePath in imagePaths:
-        try:
-            pilImage = Image.open(imagePath).convert('L')
-            imageNp = np.array(pilImage, 'uint8')
-            Id = int(os.path.split(imagePath)[-1].split(".")[1])
-            #print(Id)
-            name = str(os.path.split(imagePath)[-1].split(".")[2])
-            #print(name)
-            age = int(os.path.split(imagePath)[-1].split(".")[3])
-            # print (age)
-            faces = detector.detectMultiScale(imageNp)
-            for (x, y, w, h) in faces:
-                faceSamples.append(imageNp[y:y + h, x:x + w])
-                Ids.append(Id)
-                #print(Ids)
-                names.append(name)
-                #print(names)
-                ages.append(age)
-                #print(ages)
-        except Exception as e:
-            print(f"Error processing image {imagePath}: {e}")
-    return faceSamples, Ids
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 
-faces, Ids = getImagesAndLabels('../dataset')
-recognizer.train(faces, np.array(Ids))
-recognizer.save("../models/trained_lbph_face_recognizer_model.yml")
+# CAMERA can be 0 or 1 based on default camera of your computer
+camera = cv2.VideoCapture(0)
+
+while True:
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Make the image a numpy array and reshape it to the models input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    # Listen to the keyboard for presses.
+    keyboard_input = cv2.waitKey(1)
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
