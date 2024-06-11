b3a74a80adf1f413f6b627abaafce72718ef9441
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-11 15:59:19 +0200

    sign and face recognition

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
@@ -5,8 +6,11 @@ import numpy as np
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
+# Get the absolute path of the model file
+model_path = os.path.abspath("keras_Model.h5")
+
 # Load the model
-model = load_model("keras_Model.h5", compile=False)
+model = load_model(model_path, compile=False)
 
 # Load the labels
 class_names = open("labels.txt", "r").readlines()
